e730e997136674255869c15c54b1c42fe9b452c2
Mohammad Robiul Hossen <robim6225@gmail.com>
2026-01-11 12:50:44 +0600

    add testimonial section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import HeroSection from "@/components/pages/home/heroSection";
 import { ServiceSection } from "@/components/pages/home/ServiceSection";
+import { TestimonialSection } from "@/components/pages/home/TestimonialSection";
 
 export default function Home() {
   return (
     <div>
       <HeroSection />
       <ServiceSection />
+      <TestimonialSection />
     </div>
   );
 }
