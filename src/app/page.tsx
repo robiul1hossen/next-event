56375c5ff385502abf44e9216595cdba7bdc5b73
Mohammad Robiul Hossen <robim6225@gmail.com>
2026-01-11 12:49:14 +0600

    add services section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import HeroSection from "@/components/pages/home/heroSection";
+import { ServiceSection } from "@/components/pages/home/ServiceSection";
 
 export default function Home() {
   return (
     <div>
       <HeroSection />
+      <ServiceSection />
     </div>
   );
 }
